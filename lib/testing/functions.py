c05befca995cfbed61f99265748682f194e0425d
Cheasy Jay <jamohgamer@gmail.com>
2024-05-22 12:08:43 +0300

    done

---
+++ lib/testing/functions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+greet_programmer()
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Naureen")
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
+
+def add(num1, num2):
+    print(num1 + num2)
+
+add(1, 2)
+
+def halve(number):
+    print(number/2)
+    # return number
+halve(74)
